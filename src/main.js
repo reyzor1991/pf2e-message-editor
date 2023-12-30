9a62a777bb7dd895565b83aec1222e6d42b4e2a3
reyzor1991 <mishabudjakov@gmail.com>
2023-12-30 10:22:02 +0200

    base feature

--- src/main.js
+++ src/main.js
@@ -1,3 +1,62 @@
-Hooks.on('init', function() {
-    ui.notifications.info("Hello from pf2e-message-editor");
+const moduleName = "pf2e-message-editor"
+
+class MessageEditor extends FormApplication {
+
+    constructor(options) {
+        super();
+        this.message = options.message;
+    }
+
+    async getData() {
+        const rollData = { ...this.message?.item?.getRollData(), ...this.message?.actor?.getRollData() };
+
+        return mergeObject(super.getData(), {
+            content: await TextEditor.enrichHTML(this.message.content, {
+                rollData,
+                secrets: this.message.isOwner,
+            }),
+            editable: this.isEditable,
+            owner: this.message.isOwner,
+        });
+    }
+
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            title: "Message Editor",
+            id: `${moduleName}`,
+            classes: [moduleName],
+            template: "modules/pf2e-message-editor/templates/message.hbs",
+            width: 500,
+            height: "auto",
+            resizable: true,
+        });
+    }
+
+    async _updateObject(_event, data) {
+        await this.message.update({
+            content: data.content,
+            timestamp: Date.now()
+        })
+    }
+
+    activateListeners($html) {
+        super.activateListeners($html);
+    }
+}
+
+Hooks.on("renderChatMessage", async (message, html) => {
+    html.find('.message-metadata .message-delete').before(`<a aria-label="Edit" class="message-edit"><i class="fas fa-edit"></i></a>`)
+
+    html.on('click', '.message-metadata .message-edit', function() {
+        new MessageEditor({message}).render(true)
+    })
 })
+
+Hooks.on("init", () => {
+    const module = game.modules.get(moduleName)
+    module.api = {
+        form: {
+            MessageEditor,
+        },
+    }
+});
